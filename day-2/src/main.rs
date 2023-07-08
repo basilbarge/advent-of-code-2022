78d7a56d6fdc93355c8ef21e5cda2e7bd430e83a
Zach Barge <zachb.barge@gmail.com>
2023-07-08 00:12:24 -0400

    Identified each players move for every round.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,3 +1,25 @@
+const ROCK: u32 = 1;
+const PAPER: u32 = 2;
+const SCISSORS: u32 = 3;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let playbook: Vec<&str> = include_str!("./input.txt").lines().collect();
+
+    let mut my_move: Vec<&str> = Vec::new();
+    let mut opponent_move: Vec<&str> = Vec::new();
+
+    for play in playbook {
+        let round = play.split(" ").collect::<Vec<&str>>();
+
+        my_move.push(round[0]);
+        opponent_move.push(round[1]);
+    }
+
+    let score = 0;
+    my_move.iter().enumerate().for_each(|(i, round_move)| {
+        
+    })
+
 }
