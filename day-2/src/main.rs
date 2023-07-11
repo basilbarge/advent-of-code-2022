68401eea9c433eafb24905809c9f9e9f69c5e7e3
Zach Barge <zachb.barge@gmail.com>
2023-07-11 12:37:43 -0400

    Began refactoring code. Added round struct and refactored to use chars rather than &str.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -5,6 +5,12 @@ const LOST: u32 = 0;
 const DRAW: u32 = 3;
 const WIN: u32 = 6;
 
+struct Round {
+    player_move: char,
+    opponent_move: char,
+    outcome: bool,
+}
+
 fn main() {
 
     let playbook: Vec<&str> = include_str!("./input.txt").lines().collect();
@@ -13,7 +19,7 @@ fn main() {
     let mut opponent_move: Vec<&str> = Vec::new();
 
     for play in playbook {
-        let round = play.split(" ").collect::<Vec<&str>>();
+        let round = play.split(" ").collect::<Vec<char>>();
 
         opponent_move.push(round[0]);
         my_move.push(round[1]);
@@ -22,39 +28,39 @@ fn main() {
     let mut score: u32 = 0;
     my_move.iter().enumerate().for_each(|(i, round_move)| {
         // Both players choose Rock
-        if round_move == &"X" && opponent_move.get(i).unwrap() == &"A" {
+        if round_move == 'X' && opponent_move.get(i).unwrap() == 'A' {
             score += DRAW + ROCK;
         }
         // Both players chose Paper
-        if round_move == &"Y" && opponent_move.get(i).unwrap() == &"B" {
+        if round_move == 'Y' && opponent_move.get(i).unwrap() == 'B' {
             score += DRAW + PAPER;
         }
         // Both players chose scissors
-        if round_move == &"Z" && opponent_move.get(i).unwrap() == &"C" {
+        if round_move == 'Z' && opponent_move.get(i).unwrap() == 'C' {
             score += DRAW + SCISSORS;
         }
         // I chose rock, oppenent chose paper
-        if round_move == &"X" && opponent_move.get(i).unwrap() == &"B" {
+        if round_move == 'X' && opponent_move.get(i).unwrap() == 'B' {
             score += LOST + ROCK;
         }
         // I chose rock, opponent chose scissors
-        if round_move == &"X" && opponent_move.get(i).unwrap() == &"C" {
+        if round_move == 'X' && opponent_move.get(i).unwrap() == 'C' {
             score += WIN + ROCK;
         }
         // I chose paper, opponent chose rock
-        if round_move == &"Y" && opponent_move.get(i).unwrap() == &"A" {
+        if round_move == 'Y' && opponent_move.get(i).unwrap() == 'A' {
             score += WIN + PAPER;
         }
         // I chose paper, opponent chose scissors
-        if round_move == &"Y" && opponent_move.get(i).unwrap() == &"C" {
+        if round_move == 'Y' && opponent_move.get(i).unwrap() == 'C' {
             score += LOST + PAPER;
         }
         // I chose scissors, opponent chose rock
-        if round_move == &"Z" && opponent_move.get(i).unwrap() == &"A" {
+        if round_move == 'Z' && opponent_move.get(i).unwrap() == 'A' {
             score += LOST + SCISSORS;
         }
         // I chose scissors, opponent chose paper
-        if round_move == &"Z" && opponent_move.get(i).unwrap() == &"B" {
+        if round_move == 'Z' && opponent_move.get(i).unwrap() == 'B' {
             score += WIN + SCISSORS;
         }
     });
@@ -65,39 +71,39 @@ fn main() {
     let mut score: u32 = 0;
     my_move.iter().enumerate().for_each(|(i, round_move)| {
         // Both players choose Rock
-        if round_move == &"X" && opponent_move.get(i).unwrap() == &"A" {
+        if round_move == 'X' && opponent_move.get(i).unwrap() == 'A' {
             score += DRAW + ROCK;
         }
         // Both players chose Paper
-        if round_move == &"Y" && opponent_move.get(i).unwrap() == &"B" {
+        if round_move == 'Y' && opponent_move.get(i).unwrap() == 'B' {
             score += DRAW + PAPER;
         }
         // Both players chose scissors
-        if round_move == &"Z" && opponent_move.get(i).unwrap() == &"C" {
+        if round_move == 'Z' && opponent_move.get(i).unwrap() == 'C' {
             score += DRAW + SCISSORS;
         }
         // I chose rock, oppenent chose paper
-        if round_move == &"X" && opponent_move.get(i).unwrap() == &"B" {
+        if round_move == 'X' && opponent_move.get(i).unwrap() == 'B' {
             score += LOST + ROCK;
         }
         // I chose rock, opponent chose scissors
-        if round_move == &"X" && opponent_move.get(i).unwrap() == &"C" {
+        if round_move == 'X' && opponent_move.get(i).unwrap() == 'C' {
             score += WIN + ROCK;
         }
         // I chose paper, opponent chose rock
-        if round_move == &"Y" && opponent_move.get(i).unwrap() == &"A" {
+        if round_move == 'Y' && opponent_move.get(i).unwrap() == 'A' {
             score += WIN + PAPER;
         }
         // I chose paper, opponent chose scissors
-        if round_move == &"Y" && opponent_move.get(i).unwrap() == &"C" {
+        if round_move == 'Y' && opponent_move.get(i).unwrap() == 'C' {
             score += LOST + PAPER;
         }
         // I chose scissors, opponent chose rock
-        if round_move == &"Z" && opponent_move.get(i).unwrap() == &"A" {
+        if round_move == 'Z' && opponent_move.get(i).unwrap() == 'A' {
             score += LOST + SCISSORS;
         }
         // I chose scissors, opponent chose paper
-        if round_move == &"Z" && opponent_move.get(i).unwrap() == &"B" {
+        if round_move == 'Z' && opponent_move.get(i).unwrap() == 'B' {
             score += WIN + SCISSORS;
         }
     });
