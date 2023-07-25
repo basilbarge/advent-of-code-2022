65dce9c5263767d36e12efedb63fd341bc61b74f
Zach Barge <zachb.barge@gmail.com>
2023-07-24 20:04:22 -0400

    Succesfully completed part 2 of the day 2 challenge.

--- day-2/src/part-2.rs
+++ day-2/src/part-2.rs
@@ -3,18 +3,42 @@ fn main() {
     let playbook: Vec<&str> = include_str!("./input.txt").lines().collect();
 
     let mut entries: Vec<char> = Vec::new();
+    let mut opponent_moves: Vec<Move> = Vec::new();
+    let mut outcomes: Vec<Outcome> = Vec::new();
 
     for play in playbook {
-        play.split_whitespace().for_each(|set| {
-            entries.push(set.chars().last().unwrap());
-        })
+        let first = play.split_whitespace().nth(0).unwrap().chars().last().unwrap();
+        let second = play.split_whitespace().last().unwrap().chars().last().unwrap();
+        let count = play.split_whitespace().count();
+
+        match  first {
+            'A' => opponent_moves.push(Move::Rock),
+            'B' => opponent_moves.push(Move::Paper),
+            'C' => opponent_moves.push(Move::Scissors),
+            _ => panic!("Move type not supported")
+        }
+
+        match second {
+            'X' => outcomes.push(Outcome::Lose),
+            'Y' => outcomes.push(Outcome::Draw),
+            'Z' => outcomes.push(Outcome::Win),
+            _ => panic!("Outcome type not supported")
+        }
     }
 
-    for entry in entries {
-        println!("{entry}");
+    let mut sum = 0;
+    for (i, outcome) in outcomes.iter().enumerate() {
+        let my_move = Move::determine_my_move((opponent_moves.get(i).unwrap(), outcome));
+
+        sum += outcome.calculate_score(&my_move);
     }
+
+
+    println!("My total score is {sum}");
+
 }
 
+#[derive(Debug)]
 enum Move {
     Rock,
     Paper,
@@ -22,27 +46,24 @@ enum Move {
 }
 
 impl Move {
-    fn determine_outcome(&self, other_move: &Move) -> Outcome {
-        match &self {
-            Move::Rock => match other_move {
-                Move::Rock => Outcome::Draw,
-                Move::Paper => Outcome::Lose,
-                Move::Scissors => Outcome::Win
-            },
-            Move::Paper =>  match other_move {
-                Move::Rock => Outcome::Win,
-                Move::Paper => Outcome::Draw,
-                Move::Scissors => Outcome::Lose
-            },
-            Move::Scissors => match other_move {
-                Move::Rock => Outcome::Lose,
-                Move::Paper => Outcome::Win,
-                Move::Scissors => Outcome::Draw
-            }
-        }
+    fn determine_my_move(round: (&Move, &Outcome)) -> Move {
+       match round {
+            (Move::Rock, Outcome::Win) => Move::Paper,
+            (Move::Rock, Outcome::Draw) => Move::Rock,
+            (Move::Rock, Outcome::Lose) => Move::Scissors,
+            (Move::Paper, Outcome::Win) => Move::Scissors,
+            (Move::Paper, Outcome::Draw) => Move::Paper,
+            (Move::Paper, Outcome::Lose) => Move::Rock,
+            (Move::Scissors, Outcome::Win) => Move::Rock,
+            (Move::Scissors, Outcome::Draw) => Move::Scissors,
+            (Move::Scissors, Outcome::Lose) => Move::Paper,
+            _ => Move::Rock
+
+       }
     }
 }
 
+#[derive(Debug)]
 enum Outcome {
     Win,
     Lose,
