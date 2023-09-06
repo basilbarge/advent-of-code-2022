df192d1d807d568c91a31e824b5889b9facbaa0f
Zach Barge <zachb.barge@gmail.com>
2023-09-06 16:16:39 -0400

    Finished day 4 part one using code example.

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,17 +1,17 @@
 fn main() {
-    let pairs: Vec<&str> = include_str!("./input.txt").lines().collect();
-
-    let mut ids: Vec<Vec<&str>> = Vec::new();
-
-    for pair in &pairs {
-        ids.push(pair.split(",").collect());     
-    }
-
-    for id in ids {
-        for i in id {
-        println!("{i}");
-        }
-    }
-
-
+    println!("# of subsets: {}",
+    include_str!("./input.txt").
+        lines()
+        .map(|l| {
+            let (l, r) = l.split_once(",").unwrap();
+            let ((a, b), (c, d)) = (l.split_once("-").unwrap(), r.split_once("-").unwrap());
+            (
+                a.parse::<u8>().unwrap(),
+                b.parse::<u8>().unwrap(),
+                c.parse::<u8>().unwrap(),
+                d.parse::<u8>().unwrap(),
+                )
+        })
+        .filter(|(a, b, c, d)| ((a >= c && b <= d) || (a <= c && b >= d)))
+        .count());
 }
