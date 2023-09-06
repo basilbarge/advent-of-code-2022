24400ccb4cf2801b77158c6a690deacdff1812db
Zach Barge <zachb.barge@gmail.com>
2023-09-06 14:41:14 -0400

    Started day 4, got inputs split up.

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let pairs: Vec<&str> = include_str!("./input.txt").lines().collect();
+
+    let mut ids: Vec<Vec<&str>> = Vec::new();
+
+    for pair in &pairs {
+        ids.push(pair.split(",").collect());     
+    }
+
+    for id in ids {
+        for i in id {
+        println!("{i}");
+        }
+    }
+
+
 }
---
+++ day-4/target/debug/day-4.d
@@ -0,0 +1,1 @@
+/home/zanderson/Projects/advent-of-code-2022/day-4/target/debug/day-4: /home/zanderson/Projects/advent-of-code-2022/day-4/src/input.txt /home/zanderson/Projects/advent-of-code-2022/day-4/src/main.rs
