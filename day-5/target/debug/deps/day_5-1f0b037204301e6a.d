18949f83230a738c409531e2398dab2c56762507
Zach Barge <zachb.barge@gmail.com>
2023-10-10 19:18:17 -0400

    Created project for day 5 problem.

---
+++ day-5/target/debug/deps/day_5-1f0b037204301e6a.d
@@ -0,0 +1,5 @@
+/home/basilbarge/Projects/advent-of-code-2022/day-5/target/debug/deps/day_5-1f0b037204301e6a.rmeta: src/main.rs
+
+/home/basilbarge/Projects/advent-of-code-2022/day-5/target/debug/deps/day_5-1f0b037204301e6a.d: src/main.rs
+
+src/main.rs:
---
+++ day-5/target/debug/deps/day_5-7b76871df6e78b38.d
@@ -0,0 +1,5 @@
+/home/basilbarge/Projects/advent-of-code-2022/day-5/target/debug/deps/day_5-7b76871df6e78b38.rmeta: src/main.rs
+
+/home/basilbarge/Projects/advent-of-code-2022/day-5/target/debug/deps/day_5-7b76871df6e78b38.d: src/main.rs
+
+src/main.rs:
